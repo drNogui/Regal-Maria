d0760416583b349231b5152ee4252bd82969a18b
Nogui Labs <soporte@sportandapps.com>
2024-11-25 23:31:21 +0100

    canvis

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// Puedes agregar cualquier funcionalidad adicional aquí
-console.log('Mi Web Simple está cargada');
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.image-grid img');
+
+    images.forEach((img, index) => {
+        img.addEventListener('click', () => {
+            img.classList.toggle('flipped');
+            setTimeout(() => {
+                img.src = img.classList.contains('flipped') ? `images/OtherImage${index + 1}.png` : 'images/Map.png';
+            }, 300); // Ajusta el tiempo según la duración de la transición
+        });
+    });
+});
